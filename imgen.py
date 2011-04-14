517c04373cd9efa60513bdfe3b0cdf2f3fbc5117
Liam <millcush@gmail.com>
2011-04-14 15:08:16 -0400

    Metatron.

--- imgen.py
+++ imgen.py
@@ -4,13 +4,19 @@ import sys
 import urllib2
 import re
 
+selection = [int(x) for x in sys.argv[2:]]
+
 solutions = open(sys.argv[1], 'r')
 s = filter(None, [line.strip() for line in solutions])
 
 while s:
-  url = 'http://man.ozwebwiz.com'+s.pop(1)
+  name = s.pop(0)
+  code = s.pop(0)
+  url = 'http://man.ozwebwiz.com'+code
   lvl = re.search('lvl=([0-9]+)', url).group(0)[4:]
+  if not int(lvl) in selection:
+    continue
   print "Generating {0}".format(lvl)
   image = urllib2.urlopen(url)
-  with open("{0}_{1}.png".format(lvl, s.pop(0).replace(' ', '_')), 'w+') as f:
+  with open("{0}_{1}.png".format(lvl, name.replace(' ', '_')), 'w+') as f:
     f.write(image.read())
